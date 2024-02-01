eabab0c6aec7dc6a8abf25a0a6a59b5cdbd71d3e
Daniel Ramos <daniel.ramosg@gmail.com>
2024-02-01 12:31:37 +0100

    Add dropdown menu for radix and fix some layout

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,6 +7,7 @@ declare global {
     W: wallet;
     d3: typeof d3;
     price: number;
+    radix: number;
   }
 }
 
@@ -14,6 +15,7 @@ let mode = "junior";
 let maxPrice = 200;
 
 window.price = 145;
+window.radix = 4;
 
 // let W: wallet;
 
@@ -23,16 +25,16 @@ function makeWallet() {
   document.getElementById("wallet")?.remove();
   document.getElementById("walletValue")?.remove();
 
-  let radix = Math.max(
-    Number((document.getElementById("radix") as HTMLInputElement).value),
-    2
-  );
+  // let radix = Math.max(
+  //   Number((document.getElementById("radix") as HTMLInputElement).value),
+  //   2
+  // );
 
   // calculate number of pockets
   let i = 0;
-  for (i = 0; radix ** i < maxPrice; i += 1) {}
+  for (i = 0; window.radix ** i < maxPrice; i += 1) {}
 
-  window.W = new wallet(radix, i);
+  window.W = new wallet(window.radix, i);
 }
 
 function newPrize() {
@@ -42,6 +44,33 @@ function newPrize() {
 
   window.price = prizes[i].price;
 }
+
+function changeRadix(b: number) {
+  window.radix = b;
+  makeWallet();
+}
+
+/* MAIN SETUP */
+
+const radixOptions = [2, 3, 4, 5, 10];
+const radixMenu = d3
+  .select("#radix-menu")
+  .append("div")
+  .classed("dropdown", true);
+
+radixMenu
+  .append("svg")
+  .append("image")
+  .attr("href", new URL("../svg/coin.svg#svg1", import.meta.url).href);
+
+radixMenu
+  .append("ul")
+  .selectAll("li")
+  .data(radixOptions)
+  .enter()
+  .append("li")
+  .text((d) => d)
+  .on("click", (ev, d) => changeRadix(d));
 
 d3.select("#radix").on("change", makeWallet);
 
